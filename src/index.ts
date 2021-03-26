f146d8f4be364e1665f656a19d41dcc7fddb1dff
PratikDey <56970651+PratikDey@users.noreply.github.com>
2021-03-26 23:58:33 +0530

    added mikro-orm since it is easy to get started with, will change to typeorm at production

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,45 @@
-import { ApolloServer } from "apollo-server-express";
-import "reflect-metadata";
-import * as Express from "express";
-import { buildSchema, Query, Resolver } from "type-graphql";
+// import { ApolloServer } from "apollo-server-express";
+// import "reflect-metadata";
+// import Express from "express";
+// import { buildSchema, Query, Resolver } from "type-graphql";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
-@Resolver()
-class HelloResolver {
-  @Query(() => String)
-  async hello() {
-    return "Hello Graphql";
-  }
-}
+// @Resolver()
+// class HelloResolver {
+//   @Query(() => String)
+//   async hello() {
+//     return "Hello Graphql";
+//   }
+// }
 
 const main = async () => {
-  const schema = await buildSchema({
-    resolvers: [HelloResolver],
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "blogsite",
+    type: "postgresql",
+    debug: !__prod__,
   });
-  const apolloServer = new ApolloServer({ schema });
 
-  const app = Express();
+  const post = orm.em.create(Post, {
+    title: "First Post",
+  }); //creates the post with a specific title
 
-  apolloServer.applyMiddleware({ app });
+  orm.em.persistAndFlush(post); //adds the post to the database
 
-  app.listen(4000, () => {
-    console.log("server started on http://localhost:4000/graphql");
-  });
+  // const schema = await buildSchema({
+  //   resolvers: [HelloResolver],
+  // });
+  // const apolloServer = new ApolloServer({ schema });
+
+  // const app = Express();
+
+  // apolloServer.applyMiddleware({ app });
+
+  // app.listen(4000, () => {
+  //   console.log("server started on http://localhost:4000/graphql");
+  // });
 };
 
 main();
