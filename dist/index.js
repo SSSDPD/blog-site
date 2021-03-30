92e184d5aff4ac2a560be682f2a5ef0eae098e5c
PratikDey <56970651+PratikDey@users.noreply.github.com>
2021-03-30 19:55:10 +0530

    added the change in package.json -from js -to ts

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,14 +21,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_express_1 = require("apollo-server-express");
+require("reflect-metadata");
+const express_1 = __importDefault(require("express"));
+const type_graphql_1 = require("type-graphql");
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+let HelloResolver = class HelloResolver {
+    hello() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return "Hello Graphql";
+        });
+    }
+};
+__decorate([
+    type_graphql_1.Query(() => String),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", Promise)
+], HelloResolver.prototype, "hello", null);
+HelloResolver = __decorate([
+    type_graphql_1.Resolver()
+], HelloResolver);
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const posts = yield orm.em.find(Post_1.Post, {});
-    console.log(posts);
+    const schema = yield type_graphql_1.buildSchema({
+        resolvers: [HelloResolver],
+    });
+    const apolloServer = new apollo_server_express_1.ApolloServer({ schema });
+    const app = express_1.default();
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log("server started on http://localhost:4000/graphql");
+    });
 });
 main().catch((error) => {
     console.log(error);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-// import { ApolloServer } from "apollo-server-express";
-// import "reflect-metadata";
-// import Express from "express";
-// import { buildSchema, Query, Resolver } from "type-graphql";
+import { ApolloServer } from "apollo-server-express";
+import "reflect-metadata";
+import Express from "express";
+import { buildSchema, Query, Resolver } from "type-graphql";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
-// @Resolver()
-// class HelloResolver {
-//   @Query(() => String)
-//   async hello() {
-//     return "Hello Graphql";
-//   }
-// }
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello Graphql";
+  }
+}
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -24,21 +24,21 @@ const main = async () => {
 
   // await orm.em.persistAndFlush(post); //adds the post to the database
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 
-  // const schema = await buildSchema({
-  //   resolvers: [HelloResolver],
-  // });
-  // const apolloServer = new ApolloServer({ schema });
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+  const apolloServer = new ApolloServer({ schema });
 
-  // const app = Express();
+  const app = Express();
 
-  // apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app });
 
-  // app.listen(4000, () => {
-  //   console.log("server started on http://localhost:4000/graphql");
-  // });
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000/graphql");
+  });
 };
 
 main().catch((error) => {
