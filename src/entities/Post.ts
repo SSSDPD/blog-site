a8116873a6912be0e2a08312b81dc556ccb3dbe7
PratikDey <56970651+PratikDey@users.noreply.github.com>
2021-05-05 11:29:18 +0530

    added relations and added middleware

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -38,6 +38,10 @@ export class Post extends BaseEntity {
   embed: string;
 
   @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  @Field()
   @Column()
   creatorId: number;
 
