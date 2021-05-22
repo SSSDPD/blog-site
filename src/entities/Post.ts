5c28f5e81ccd2f1dd2f08f5674ea2927649c2531
Souvik Srimani <55970375+Rotten-Banana@users.noreply.github.com>
2021-05-23 03:42:12 +0530

    upvote downvote done. if u can please add the caching

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -5,9 +5,11 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { Upvote } from "./Upvote";
 import { User } from "./User";
 
 @ObjectType()
@@ -35,6 +37,9 @@ export class Post extends BaseEntity {
 
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
+
+  @OneToMany(() => Upvote, (upvote) => upvote.post)
+  upvote: Upvote[];
 
   @Field()
   featureImage: string;
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -4,6 +4,7 @@ import {
   Ctx,
   Field,
   InputType,
+  Int,
   Mutation,
   Query,
   Resolver,
@@ -12,6 +13,8 @@ import {
 
 import { MyContext } from "../types";
 import { isAuth } from "../middleware/isAuth";
+import { getConnection } from "typeorm";
+import { Upvote } from "../entities/Upvote";
 
 @InputType()
 class PostInput {
@@ -43,6 +46,62 @@ export class PostResolver {
     return Post.create({ ...input, creatorId: req.session.userId }).save();
   }
 
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async vote(
+    @Arg("postid", () => Int) postId: number,
+    @Arg("value", () => Int) value: number,
+    @Ctx() { req }: MyContext
+  ) {
+    const isUpvote = value !== -1;
+    const realValue = isUpvote ? 1 : -1;
+    const { userId } = req.session;
+    const upvote = await Upvote.findOne({ where: { postId, userId } });
+
+    if (upvote && upvote.value !== realValue) {
+      // have voted already and changing vote
+      await getConnection().transaction(async (tm) => {
+        tm.query(
+          `
+        update upvote
+        set value = $1
+        where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
+
+        await tm.query(
+          `
+        update post
+        set points = points + $1
+        where id = $2;
+        `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!upvote) {
+      // have never voted
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+        insert into upvote ("userId","postId","value")
+        values ($1,$2,$3)
+        `,
+          [userId, postId, realValue]
+        );
+
+        await tm.query(
+          `
+        update post
+        set points = points + $1
+        where id = $2;
+        `,
+          [realValue, postId]
+        );
+      });
+    }
+    return true;
+  }
   @Mutation(() => Post, { nullable: true })
   async updatePost(
     @Arg("id") id: number,
